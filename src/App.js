48dd0b8761cef65b6eb0cde3566951ab153f3b03
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-17 15:41:14 +0100

    added palette

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Palette from './components/Palette';
+import seedColors from './seedColors';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+     <Palette {...seedColors} />
     </div>
   );
 }
