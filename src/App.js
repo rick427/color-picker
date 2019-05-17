a001f929385cad12d2ef229292a8456069bfef02
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-17 16:08:37 +0100

    styling colorbox component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import seedColors from './seedColors';
 function App() {
   return (
     <div>
-     <Palette {...seedColors[5]} />
+     <Palette {...seedColors[2]} />
     </div>
   );
 }
--- src/components/Colorbox.js
+++ src/components/Colorbox.js
@@ -3,9 +3,16 @@ import './colorbox.css';
 
 class Colorbox extends Component {
   render() {
+    const {background, name} = this.props;
     return (
-      <div style={{background: this.props.background}} className="colorbox">
-        <span>{this.props.name}</span>
+      <div style={{background}} className="colorbox">
+       <div className="copy-container">
+         <div className="box-content">
+           <span>{name}</span>
+         </div>
+         <button className="copy-button">Copy</button>
+         <span className="see-more">More</span>
+       </div>
       </div>
     )
   }
