b197e7421eb8c4f9b58d53c2a24879b7b289a70e
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-18 17:08:00 +0100

    latest commit

--- src/components/Palette.js
+++ src/components/Palette.js
@@ -7,24 +7,30 @@ class Palette extends Component {
   constructor(props){
     super(props);
     this.state = {
-      level: 500
+      level: 500,
+      format: "hex"
     };
     this.changeLevel=this.changeLevel.bind(this);
+    this.changeFormat=this.changeFormat.bind(this);
   }
 
   changeLevel(level){
     this.setState({level});
   }
 
+  changeFormat(val){
+    this.setState({format: val})
+  }
+
   render() {
     const {colors} = this.props.palette;
-    const {level} = this.state;
+    const {level, format} = this.state;
     const colorboxes = colors[level].map(color => (
-        <Colorbox background={color.hex} name={color.name} />
+        <Colorbox background={color[format]} name={color.name} />
     ))
     return (
       <div className="palette">
-        <Navbar level={level} changeLevel={this.changeLevel} />
+        <Navbar level={level} changeLevel={this.changeLevel} handleChange={this.changeFormat} />
         {/*Navbar foes here*/}
         <div className="palette-colors">
           {colorboxes}
