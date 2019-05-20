91b5a3377fd92ea26b079aa5ecbf91288f3560cc
rick427 <44168932+rick427@users.noreply.github.com>
2019-05-20 09:08:18 +0100

    adding router and basic routes

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Palette from './components/Palette';
 import seedColors from './seedColors';
 import {generatePalette} from './colorHelpers';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   console.log(generatePalette(seedColors[2]))
   return (
-    <div>
-     <Palette palette={generatePalette(seedColors[2])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette list goes here</h1>}/>
+      <Route exact path="/palette/:id" render={() => <h1>Individula components</h1>} />
+    </Switch>
+    // <div>
+    //  <Palette palette={generatePalette(seedColors[2])} />
+    // </div>
   );
 }
 
